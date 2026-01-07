f57ee6288a252d0d2af592e8715d3ac464dcf818
Luis Fernando Fernández de Gante <luisfernandezdegante58@gmail.com>
2026-01-07 17:22:03 -0600

    20260107ProyectoConsola
    Proyecto creado en c# de tipo consola que conecta sqlserver  . NET

---
+++ practica2LFFG/practica2LFFG/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace practica2LFFG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Busqueda de Prestamos-------------");
+            Console.WriteLine("Escribe el ID de la Prestación");
+            string prestamo = Console.ReadLine();
+
+
+            //Cadena de conexion 
+            string connectionString = "Server=localhost; Database=BD_BIBLIOTECA; Trusted_Connection=True;";
+
+            //Consulta SQL (BUSCA PRESTAMO)
+            string query = "SELECT TOP 5 PrestamosID, LibrosID, SociosID " +
+                            "FROM Prestamos " +
+                            "WHERE PrestamosID=@prestamo";
+
+            using (SqlConnection connection = new SqlConnection(connectionString)) //PASO 1
+            using (SqlCommand command = new SqlCommand(query, connection))          //PASO 2
+            {
+                command.Parameters.AddWithValue("@prestamo", prestamo.ToUpper());  //PASO 3 
+
+                connection.Open();                                                    //PASO 4
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("\nResultados encontrados");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Prestamo num: {reader["PrestamosID"]}");  //PASO 5
+                        Console.WriteLine($"Libro num: {reader["LibrosID"]}");
+                        Console.WriteLine($"Socio num: {reader["SociosID"]}");
+                        Console.WriteLine("---------------------\n\n");
+
+
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("No se encontro la consulta");                //
+                
+
+
+            }
+
+            Console.WriteLine("Presiona una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+    }
+}
