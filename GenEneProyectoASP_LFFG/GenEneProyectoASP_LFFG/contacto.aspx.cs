23271d82e828201f3cec680a8c083fa4cc258048
Luis Fernando Fernández de Gante <luisfernandezdegante58@gmail.com>
2026-01-10 08:08:45 -0600

    20260110_Proyecto3_BibiotecaXML_V01

---
+++ GenEneProyectoASP_LFFG/GenEneProyectoASP_LFFG/contacto.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class contacto : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //recuperacion de los datos mandados en el formulario
+        string from = Request.Form["contact_email"].ToString();
+        string nombre = Request.Form["contact_name"].ToString();
+        string personas = Request.Form["contact_personas"].ToString();
+        string extra = Request.Form["contact_adicionales"].ToString();
+        string fecha = Request.Form["contact_fecha"].ToString();
+        string hora = Request.Form["contact_hora"].ToString();
+        string subject = nombre + " Fecha: " + fecha + ". Hora: " + hora + ". Personas: " + (int.Parse(personas) + int.Parse(extra)).ToString();
+        string mensaje = "El cliente " + nombre + "ha realizado una reservacion para el dia: " + " Fecha: " + fecha + " a las: " + hora + " hrs para " +
+"Personas: " + (int.Parse(personas) + int.Parse(extra)).ToString();
+        //string resultado = sendGmail(from, subject, mensaje);
+        //Estatus.Text = resultado;
+        //redireccionar a http:localhost:789/Default.aspx?Id=2
+
+        Response.Write("<script>alert('RESERVACION CREADA'); window.location='/Default.aspx?Id=2';</script>");
+
+        //Response.Redirect("/Default.aspx?Id=2");
+
+    }
+    }
